7b3c0aa467577cc17f909b76be56382a01f29ee7
Sarah <sarah@sarahs-imac.home>
2020-05-05 13:45:33 -0400

    have this weird login

--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,16 @@ export default class Header extends Component {
                             <a href="about.html"> About</a>
                             <a href="#"> Insights</a>
                             <a href="#"> Investor Relations</a>
-                            <a className="login"> Login </a>
+                            <button className="login"> Login </button>
+                            <form className="login_form" >
+                                <div className="login-container"> 
+                                    <label for="uname"><b>Username</b></label>
+                                    <input type="text" placeholder="Enter Username" name="uname" required></input>
+                                    <label for="psw"><b>Password</b></label>
+                                    <input type="password" placeholder="Enter Password" name="psw" required></input>
+                                    <button type="submit">Login</button> 
+                                </div>
+                            </form>
                             <a href="contact.html"> Contact</a>
                         </li>
                     </ul>
