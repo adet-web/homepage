320666982047770ee05b15972383837e809bca79
Matthew Cen <mc6844@nyu.edu>
2020-05-05 16:12:39 -0400

    rewrite navigation to use react router

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,40 @@
-import React, {Component} from 'react';
-import logo from './img/adet_logo.png';
-import Login_Form from './login_register';
+import React, { Component } from "react";
+import logo from "./img/adet_logo.png";
+import Login_Form from "./login_register";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default class Header extends Component {
-    render() {
-      return (
-        <header> 
-            <div className="nav-unit">
-                <img className="logo" src={logo} alt='logo' width='100px' height='100px'/> 
-                <nav>
-                    <ul> 
-                        <li> 
-                            <a href="about.html"> About</a>
-                            <a href="#"> Insights</a>
-                            <a href="#"> Investor Relations</a>
-                            <a className="login"> Login </a>
-                            <a href="contact.html"> Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <div className="nav-unit">
+          <img
+            className="logo"
+            src={logo}
+            alt="logo"
+            width="100px"
+            height="100px"
+          />
+          <Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                  <Link to="/about">About</Link>
+                  <Link to="/insights">Insights</Link>
+                  <Link to="/relations">Investor Relations</Link>
+                  <Link to="/login" className="login">
+                    Login
+                  </Link>
+                  <Link to="/register">Register</Link>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </Router>
+        </div>
+      </header>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,47 +1,61 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import css from './css/styles.scss';
-import Header from './Header';
-import Footer from './Footer'
-import CodingImage from './img/coding-image-new.png'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import css from "./css/styles.scss";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import CodingImage from "./img/coding-image-new.png";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-export default class Homepage extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="app">
         <div className="header">
-          <Header/>
+          <Header />
         </div>
-        <img className="coding-image" src={CodingImage} alt="coding image" width='100%' height='250vw'/>
-        <div className="main-page-container">
-            <div className="row_1"> 
-                <div className="welcome">
-                    <h1> Welcome to A.D.E.T! </h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo eget magna fermentum iaculis. Commodo elit at imperdiet dui accumsan sit amet. Senectus et netus et malesuada fames. Nibh praesent tristique magna sit amet. Eros donec ac odio tempor orci dapibus. Molestie ac feugiat sed lectus vestibulum mattis. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Vulputate eu scelerisque felis imperdiet proin fermentum leo. Turpis egestas pretium aenean pharetra magna. </p>
-                </div>
-                <div className="image-container"> 
-                    <h1> Place images here </h1>
-                </div>
-            </div>
-            <div className="row_2">
-                <div className="what-we-do"> 
-                    <h1> What We Do </h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo eget magna fermentum iaculis. Commodo elit at imperdiet dui accumsan sit amet. Senectus et netus et malesuada fames. Nibh praesent tristique magna sit amet. Eros donec ac odio tempor orci dapibus. Molestie ac feugiat sed lectus vestibulum mattis. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Vulputate eu scelerisque felis imperdiet proin fermentum leo. Turpis egestas pretium aenean pharetra magna. </p>
-                </div>
-                <div className="why-us"> 
-                    <h1> Why You Should Partner with Us </h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo eget magna fermentum iaculis. Commodo elit at imperdiet dui accumsan sit amet. Senectus et netus et malesuada fames. Nibh praesent tristique magna sit amet. Eros donec ac odio tempor orci dapibus. Molestie ac feugiat sed lectus vestibulum mattis. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Vulputate eu scelerisque felis imperdiet proin fermentum leo. Turpis egestas pretium aenean pharetra magna. </p>
-                </div>
-            </div>
-        </div>
-        <div className='footer'>
-          <Footer/>
+        <img
+          className="coding-image"
+          src={CodingImage}
+          alt="coding image"
+          width="100%"
+          height="250vw"
+        />
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+            {/* <Route path="/about">
+            <About />
+            </Route>
+            <Route path="/insights">
+            <Insights />
+            </Route>
+            <Route path="/relations">
+            <Relations />
+            </Route>
+            <Route path="/login" className="login">
+            <Login />
+            </Route>
+            <Route path="/register">
+            <Register />
+            </Route>
+            <Route path="/contact">
+            <Contact />
+          </Route> */}
+          </Switch>
+        </Router>
+
+        <div className="footer">
+          <Footer />
         </div>
       </div>
     );
   }
 }
-ReactDOM.render(
-  <Homepage/>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
