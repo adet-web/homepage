75da5f634444ae39c2f3236af57dea2837d38669
Matthew Cen <mc6844@nyu.edu>
2020-05-06 18:01:48 -0400

    replace name

--- src/Footer.js
+++ src/Footer.js
@@ -20,7 +20,7 @@ export default class Footer extends Component {
                 <p> © 2020 Analyze Data Execute Trades, LLC. All Rights reserved.</p>
                 <p> 
                     This website is not an offer to, or solicitation of, any potential clients or investors 
-                    for the provision by Two Sigma of investment management, advisory or any other related services. 
+                    for the provision by ADET of investment management, advisory or any other related services. 
                     No material listed on this website is or should be construed as investment advice, nor is anything on 
                     this website an offer to sell, or a solicitation of an offer to buy, any security or other instrument. 
                     Links from this website to third-party websites do not imply any endorsement by the third party of this 
