089ee926b5ba60f4a6c674e3b4b4bc1236fe47ed
unknown <cg3113@nyu.edu>
2020-05-06 17:53:55 -0400

    added homepage fluff

--- src/Home.js
+++ src/Home.js
@@ -9,15 +9,8 @@ export default class Home extends Component {
             <h1> Welcome to A.D.E.T! </h1>
             <p>
               {" "}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo
-              eget magna fermentum iaculis. Commodo elit at imperdiet dui
-              accumsan sit amet. Senectus et netus et malesuada fames. Nibh
-              praesent tristique magna sit amet. Eros donec ac odio tempor orci
-              dapibus. Molestie ac feugiat sed lectus vestibulum mattis.
-              Ultricies lacus sed turpis tincidunt id aliquet risus feugiat.
-              Vulputate eu scelerisque felis imperdiet proin fermentum leo.
-              Turpis egestas pretium aenean pharetra magna.{" "}
+              	We are project ADET, a team consisting of anti-social nerds who just happened to form a god tier alpha squad team in our senior/junior year at NYU Tandon University. Led by our alpha omega chad, Bryan, his lackeys and him have come together to create a website for the purpose of helping any casual person who does not know much about how to start investing to start with our website. Our site provides the simple stock price prediction algorithms that are simple to use and view to the everyday person. One simple click of our algorithm will provide the user with the power to see future stock forecasts generated from special algorithmic techniques that allow for such predictions to be made. Starting in 2020, we as a team have decided to put forward our dream of such a website that was simple to use, and available to everyday people. For more advanced stock traders, they may like to use our hedge fund manager system to assist other people or to look for other managers who could ease the transition between normal and algorithmic trading. We also provide our own ingenious recommendations on the stocks that we believe will do better in the future with our high IQ intellectual minds with the assistance of the same algorithms provided to you! The user! Customer satisfaction is our number 1 priority, next to satisfying our chad god, Strauss, in this god tier project, and if you have any questions, please feel free to contact us at 1-800-gofuckyourself.org
+			  {" "}
             </p>
           </div>
           <div className="image-container">
@@ -29,30 +22,32 @@ export default class Home extends Component {
             <h1> What We Do </h1>
             <p>
               {" "}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo
-              eget magna fermentum iaculis. Commodo elit at imperdiet dui
-              accumsan sit amet. Senectus et netus et malesuada fames. Nibh
-              praesent tristique magna sit amet. Eros donec ac odio tempor orci
-              dapibus. Molestie ac feugiat sed lectus vestibulum mattis.
-              Ultricies lacus sed turpis tincidunt id aliquet risus feugiat.
-              Vulputate eu scelerisque felis imperdiet proin fermentum leo.
-              Turpis egestas pretium aenean pharetra magna.{" "}
+              Our team is made up of computer science geniuses who have
+	          teamed up to create community based projects that help bring
+			  people together for the purpose of creating a more connected world
+			  in a age where such undertakings is possible. Gifted with our 
+			  own individual talent we our goal is to continue to use our knowledge
+			  that have been entrusted and given to us by NYU Tandon to bring the project
+			  we can imagine to life. There is no project that we will not take part in, any 
+			  project is scaleable as long as we set our mind to it. This project (Project ADET)
+			  is one of many of our ingenius invention of bringing people together.{" "}
             </p>
           </div>
           <div className="why-us">
             <h1> Why You Should Partner with Us </h1>
             <p>
               {" "}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo
-              eget magna fermentum iaculis. Commodo elit at imperdiet dui
-              accumsan sit amet. Senectus et netus et malesuada fames. Nibh
-              praesent tristique magna sit amet. Eros donec ac odio tempor orci
-              dapibus. Molestie ac feugiat sed lectus vestibulum mattis.
-              Ultricies lacus sed turpis tincidunt id aliquet risus feugiat.
-              Vulputate eu scelerisque felis imperdiet proin fermentum leo.
-              Turpis egestas pretium aenean pharetra magna.{" "}
+              Although there is much hate and evil in this world, there always
+			  seems to be a guiding light of goodness and purity in people that never
+			  gets to shine. Our goal to connect more people to one another brings a solution
+			  to such a hardship that has been brought upon our species. Where before we were
+			  more isolated, unable to connect to one another instantly, with today's technology, 
+			  such a feat is feasible. Using our gifted genius of the holy gods that rule over us,
+			  we have the potential to create much more difficult and inspiring projects that will
+			  solve some of the world's most difficult issues. Project ADET is just one of many 
+			  projects that we have been devleoping, with many more potential projects that will 
+			  inspire generations to come. Partner with us to join us on this great endeavor to develop
+			  stimulate the greatest that people have{" "}
             </p>
           </div>
         </div>
