1fae3c5270ebf3d4ec66292d33c3c7668c1002de
Matthew Cen <mc6844@nyu.edu>
2020-05-06 22:03:09 -0400

    add pdf download links

--- src/ClientPortalPage.js
+++ src/ClientPortalPage.js
@@ -1,70 +1,84 @@
 import React, { Fragment } from "react";
 
 const ClientPortalPage = (props) => {
-  console.log(props.values)
-    return (
+  console.log(props.values);
+  return (
     <Fragment>
-        <div className="portfolio">
-          <h1>Portfolio</h1>
-          <div>Portfolio Value:</div>
-          <div>Securities Owned:</div>
-          <form>
-            <label htmlFor="ticker">Buy:</label><br />
-            <input
-              type="text"
-              id="ticker"
-              name="ticker"
-              placeholder="Enter Ticker Symbol"
-            /><br />
-            <label htmlFor="quantity">Quantity:</label><br />
-            <input
-              type="text"
-              id="quantity"
-              name="quantity"
-              placeholder="Enter Whole Numbers"
-            /><br /><br />
-            <input type="submit" value="Submit" />
-          </form>
-          <form>
-            <label htmlFor="ticker">Sell:</label><br />
-            <input
-              type="text"
-              id="ticker"
-              name="ticker"
-              placeholder="Enter Ticker Symbol"
-            /><br />
-            <label htmlFor="quantity">Quantity:</label><br />
-            <input
-              type="text"
-              id="quantity"
-              name="quantity"
-              placeholder="Enter Whole Numbers"
-            /><br /><br />
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-        <div className="hedgefund-manager">
-          <h1>Your Hedgefund Manager</h1>
-    <div>Name: {props.values.managerName}</div>
-          <div>Email: {props.values.managerEmail}</div>
-        </div>
-        <div className="gain-insights">
-          <h1>Gain Insights</h1>
-          <p>
-            Click on the stock you are interested in and we'll give you a pdf
-            with our analysis.
-          </p>
-
+      <div className="portfolio">
+        <h1>Portfolio</h1>
+        <div>Portfolio Value:</div>
+        <div>Securities Owned:</div>
+        <form>
+          <label htmlFor="ticker">Buy:</label>
+          <br />
+          <input
+            type="text"
+            id="ticker"
+            name="ticker"
+            placeholder="Enter Ticker Symbol"
+          />
+          <br />
+          <label htmlFor="quantity">Quantity:</label>
+          <br />
+          <input
+            type="text"
+            id="quantity"
+            name="quantity"
+            placeholder="Enter Whole Numbers"
+          />
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+        <form>
+          <label htmlFor="ticker">Sell:</label>
+          <br />
+          <input
+            type="text"
+            id="ticker"
+            name="ticker"
+            placeholder="Enter Ticker Symbol"
+          />
+          <br />
+          <label htmlFor="quantity">Quantity:</label>
+          <br />
+          <input
+            type="text"
+            id="quantity"
+            name="quantity"
+            placeholder="Enter Whole Numbers"
+          />
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div className="hedgefund-manager">
+        <h1>Your Hedgefund Manager</h1>
+        <div>Name: {props.values.managerName}</div>
+        <div>Email: {props.values.managerEmail}</div>
+      </div>
+      <div className="gain-insights">
+        <h1>Gain Insights</h1>
+        <p>
+          Click on the stock you are interested in and we'll give you a pdf with
+          our analysis.
+        </p>
+        <a href="/api/pdf/adet_jp_morgan.pdf">
           <button>JPMorgan Chase & Co.</button>
-          <div></div>
+        </a>
+        <a href="/api/pdf/adet_tesla.pdf">
           <button>Tesla Inc.</button>
-          <div></div>
+        </a>
+        <a>
           <button>Visa Inc.</button>
-          <div></div>
+        </a>
+        <a>
           <button>Sprint Corporation</button>
-        </div>
+        </a>
+      </div>
     </Fragment>
-    )
-}
+  );
+};
 
-export default ClientPortalPage
+export default ClientPortalPage;
