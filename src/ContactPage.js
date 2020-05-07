4a6eef8708ffbd7aef598ae711d604ca30df8064
Matthew Cen <mc6844@nyu.edu>
2020-05-07 12:33:54 -0400

    merge

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -3,7 +3,6 @@ import React, { Fragment } from "react";
 const ContactPage = () => {
   return (
     <div className="main-page-container">
-<<<<<<< HEAD
       <article className="altern-bg">
         <section className="section-flex-c">
           <p>
@@ -38,39 +37,6 @@ const ContactPage = () => {
           </table>
         </section>
       </article>
-=======
-      <h1 className="contact-us-title"> How to Contact Us</h1>
-      <p className="textcolor">
-        If you have any questions or issues with the website, feel free to
-        contact any the admins through email
-      </p>
-      <table className="contact-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Bryan Cuellar</td>
-            <td>kbc302@nyu.edu</td>
-          </tr>
-          <tr>
-            <td>Matthew Cen</td>
-            <td>mc6844@nyu.edu </td>
-          </tr>
-          <tr>
-            <td>CJ Syki</td>
-            <td>cgs737@nyu.edu</td>
-          </tr>
-          <tr>
-            <td>Chris Gao</td>
-            <td>cg3113@nyu.edu</td>
-          </tr>
-        </tbody>
-      </table>
->>>>>>> eb9742a7936e9316c561ee2d53cf3b0d520592ec
     </div>
   );
 };
