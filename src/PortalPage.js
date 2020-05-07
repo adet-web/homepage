8725b1c5c0abb323594e63ed01b0d64d174a8d7e
Matthew Cen <mc6844@nyu.edu>
2020-05-06 22:14:26 -0400

    fix name

--- src/PortalPage.js
+++ src/PortalPage.js
@@ -5,27 +5,21 @@ import AppContext from "./appContext";
 
 export default class PortalPage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
-    var view
+    var view;
     if (this.props.state.userType === 1) {
       view = <ClientPortalPage values={this.props.state} />;
     } else if (this.props.state.userType === 2) {
-      view = <AdminPortalPage values={this.props.state}/>;
+      view = <AdminPortalPage values={this.props.state} />;
     }
     return (
       <div className="main-page-container">
         <div className="wrapper">
           <div className="personal-info">
-            <table>
-          <tr>
-            <td>Name</td>
-        <td>{this.props.state.name}</td>
-          </tr>
-            </table>
             <h1>Personal Information</h1>
-    <div>Name: </div>
+            <div>Name: {this.props.state.name}</div>
             <div>Email: {this.props.state.email}</div>
             <div>Address: {this.props.state.address}</div>
           </div>
