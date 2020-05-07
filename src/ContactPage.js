cd0fe016c6fe29df74cb8fdf253fe242a8f6fe84
unknown <cg3113@nyu.edu>
2020-05-06 20:44:41 -0400

    Added about page and contact page information

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -2,8 +2,10 @@ import React, { Fragment } from "react";
 
 const ContactPage = () => {
     return <p>
-        Contact Info
-    </p>
+        If you have any questions or issues with the website, feel free to contact any the admins through email
+		Bryan Cuellar - kbc302@nyu.edu       Matthew Cen - mc6844@nyu.edu
+		CJ Syki - cgs737@nyu.edu             Chris Gao - cg3113@nyu.edu           
+		
 }
 
 export default ContactPage
