975c83e731663a713228b3e82c2afcf2e5ef3b76
Matthew Cen <mc6844@nyu.edu>
2020-05-06 20:59:44 -0400

    fix html tags

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -5,7 +5,7 @@ const ContactPage = () => {
         If you have any questions or issues with the website, feel free to contact any the admins through email
 		Bryan Cuellar - kbc302@nyu.edu       Matthew Cen - mc6844@nyu.edu
 		CJ Syki - cgs737@nyu.edu             Chris Gao - cg3113@nyu.edu           
-		
+		</p>
 }
 
 export default ContactPage
