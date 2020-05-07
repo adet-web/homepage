233e92c05efc0c4e9e5d539a8a342f3fc7dd4676
Matthew Cen <mc6844@nyu.edu>
2020-05-07 13:01:42 -0400

    update register

--- src/Register.js
+++ src/Register.js
@@ -13,72 +13,74 @@ export default class RegisterForm extends Component {
     fetch("/api/register", {
       method: "POST",
       body: data,
-    }).then((res) => 
-       res.json()
-    ).then(data => {
-      console.log(data)
-      if (data.registerSuccess) {
-        // logged in
-        alert("Register successful")
-      }
-      else {
-        // failure
-        alert("Register failed")
-      }
     })
-  .catch(err => {
-      console.error(err)
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.registerSuccess) {
+          // logged in
+          alert("Register successful");
+        } else {
+          // failure
+          alert("Register failed");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
   render() {
     return (
-    <div className="main-page-container">
-
-      <form className="login_form" onSubmit={this.handleSubmit}>
-        <div className="login-container">
-          <label>
-            <b>Email</b>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              maxLength="255"
-              required
-            ></input>
-          </label>
-          <label>
-            <b>Password</b>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              maxLength="255"
-              required
-            ></input>
-          </label>
-          <label>
-            <b>Name</b>
-            <input
-              type="name"
-              placeholder="Name"
-              maxLength="255"
-              name="name"
-              required
-            ></input>
-          </label>
-          <label>
-            <b>Address</b>
-            <input
-              type="address"
-              placeholder="Address"
-              maxLength="255"
-              name="address"
-              required
-            ></input>
-          </label>
-          <button type="submit">Register</button>
-        </div>
-      </form>
+      <div className="main-page-container">
+        <article className="altern-bg">
+          <section>
+            <form className="login_form" onSubmit={this.handleSubmit}>
+              <div className="login-container">
+                <label>
+                  <b>Email</b>
+                  <input
+                    type="email"
+                    placeholder="Enter Email"
+                    name="email"
+                    maxLength="255"
+                    required
+                  ></input>
+                </label>
+                <label>
+                  <b>Password</b>
+                  <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    maxLength="255"
+                    required
+                  ></input>
+                </label>
+                <label>
+                  <b>Name</b>
+                  <input
+                    type="name"
+                    placeholder="Name"
+                    maxLength="255"
+                    name="name"
+                    required
+                  ></input>
+                </label>
+                <label>
+                  <b>Address</b>
+                  <input
+                    type="address"
+                    placeholder="Address"
+                    maxLength="255"
+                    name="address"
+                    required
+                  ></input>
+                </label>
+                <button type="submit">Register</button>
+              </div>
+            </form>
+          </section>
+        </article>
       </div>
     );
   }
